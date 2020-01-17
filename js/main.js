6d7f4998a9d2e554565acc59f23ed8b790df1874
jiacnacho <53543338+jiacnacho@users.noreply.github.com>
2020-01-17 16:38:48 -0300

    ok

--- js/main.js
+++ js/main.js
@@ -4,13 +4,13 @@
  document.addEventListener('DOMContentLoaded', function(){
 
     // MAPA 
-    var map = L.map('mapa').setView([51.505, -0.09], 13);
-
+    var map = L.map('mapa').setView([-34.6204834, -58.4698934], 13);
+    
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.marker([51.5, -0.09]).addTo(map)
+    L.marker([-34.6204834, -58.4698934]).addTo(map)
     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
     .openPopup();
     
